8f2f41331df713b0ad04b9e133aaed8aaa5d91f5
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-05-04 10:04:52 -0300

    meu primeiro codigo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<Integer> armazen = new ArrayList<Integer>();
+
+        while (true) {
+            try {
+                System.out.print("Digite um limite para o for: ");
+                int limite = scn.nextInt();
+
+
+                for (int i = 0; i <= limite; i++) {
+                    Integer value = isPar.numberIsPAr(i);
+                    if (value == null) {
+                        continue;
+                    }
+
+                    armazen.add(i);
+
+                }
+                System.out.println(armazen);
+                break;
+            } catch (Exception e) {
+                System.err.println("Cara nos estamos buscado NUMEROS pares e não LETRAS pares");
+                System.out.println();
+                scn.nextLine();
+                armazen.clear();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/org/example/isPar.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class isPar {
+    public static Integer numberIsPAr(int number){
+        if (number % 2 ==0) {
+            return number;
+        }
+        return null;
+    }
+}

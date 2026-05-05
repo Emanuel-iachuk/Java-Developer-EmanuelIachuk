a53e63d80170bb4b809d0624523a18e769604491
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-05-05 08:39:02 -0300

    comentarios

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/aula01OlaDevDojo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula01OlaDevDojo {
+    public static void main(String[] args) {
+        // Esse é um comentario
+
+        /*
+        * Esse é um comentario que pode conter varias linhas
+        */
+    }
+}

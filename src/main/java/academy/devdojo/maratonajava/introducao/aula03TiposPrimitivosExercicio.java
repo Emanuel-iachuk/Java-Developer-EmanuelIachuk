c199c89e3b46ab0407119ea6800296e430a644a8
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-05-26 21:58:25 -0300

    Tipos primitivos

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Emanuel";
+        String endereco = "Av mineira n 170";
+        int salario = 2000;
+        String data = "10/05/2022";
+
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salario de " + salario + ", na data " + data);
+    }
+
+}

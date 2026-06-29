72e28460c316dda6d2cba7ef0daa7d8d0e5c6601
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-06-29 18:34:08 -0300

    operador Xor

--- src/main/java/academy/devdojo/maratonajava/introducao/aula04Operadores.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/aula04Operadores.java
@@ -38,13 +38,16 @@ public class aula04Operadores {
 
 
         // Operadores logicos
-        // && (And) precisa que todas as alternativas sejam verdadeiras
-        // || (Or) somente uma precisa ser verdadeira
-        // ! (Not) inverte o valor verdadeiro vira falso
+        // && (And) - precisa que todas as alternativas sejam verdadeiras
+        // || (Or) - somente uma precisa ser verdadeira
+        // ! (Not) - inverte o valor verdadeiro vira falso
+        // ^ (Xor) - Disjunção Exclusiva precisa que os lados sejam DIFERENTES para ser verdadeiro
 
         System.out.println(true && false); // false(falso)
         System.out.println(true || false); // true(verdadeiro)
         System.out.println(!false); // valor invertido agora e true
+        System.out.println(true ^ true); // false(falso, pois são iguais)
+        System.out.println(true ^ false); // true(verdadeiro, pois são diferentes)
 
         // Operadores de atribuição
         // =, +=, -=, *=, /=, %=
@@ -61,5 +64,15 @@ public class aula04Operadores {
         saldoAtual %= 1000;
         System.out.println("Saldo após %= (Resto de 50 dividido por 1000): " + saldoAtual); // Saldo atual 50
 
+        // Operadores de Incremento
+        // ++, --
+
+        int contador = 0;
+        System.out.println("Contador: " + contador++); // Vai exibir o valor do contador antes de incrementar 1;
+        System.out.println("Contador valor atual: " + contador); // Agora o valor mudou;
+        System.out.println("Contador: " + ++contador); // Vai incrementar 1 primeiro e depois exibir o valor;
+        System.out.println("Contador: " + contador--); // Vai exibir o valor do contador antes de decrementar 1;
+        System.out.println("Contador valor atual: " + contador); // Agora o valor mudou;
+        System.out.println("Contador: " + --contador); // Vai decrementar 1 primeiro e depois exibir o valor;
     }
 }

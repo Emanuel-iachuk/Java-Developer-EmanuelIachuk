bbd87462eb8b3f86c417e9930c4469b3597b8b7f
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-07-14 17:57:59 -0300

    Estruturas condicionais

--- src/main/java/academy/devdojo/maratonajava/introducao/aule05EstruturasCondicionais02.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/aule05EstruturasCondicionais02.java
@@ -2,15 +2,15 @@ package academy.devdojo.maratonajava.introducao;
 
 public class aule05EstruturasCondicionais02 {
     public static void main(String[] args) {
-        int idade = -1;
-
+        int idade = 18;
+        String categoria;
         if (idade < 15) {
-            System.out.println("Infantil");
+            categoria = "Infantil";
         } else if (idade >= 15 && idade < 18) {
-            System.out.println("Juvenil");
+            categoria = "Juvenil";
         }else{
-            System.out.println("Adulto");
+            categoria = "Adulto";
         }
-
+        System.out.println(categoria);
     }
 }
